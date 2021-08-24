c6601f835b6f31926b03a72397573fede3cc9136
jimin <w2037@e-mirim.hs.kr>
2021-08-24 19:55:31 +0900

    Completion MusicService Commit

--- app/src/main/java/ks/hs/emirim/wlalsdl_04/ex14_1_4/MusicService.java
+++ app/src/main/java/ks/hs/emirim/wlalsdl_04/ex14_1_4/MusicService.java
@@ -2,15 +2,37 @@ package ks.hs.emirim.wlalsdl_04.ex14_1_4;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
 
 public class MusicService extends Service {
-    public MusicService() {
-    }
-
+    MediaPlayer mp;
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+    @Override
+    public void onCreate(){
+        Log.i("서비스 테스트 ::","onCreate() 호출됨");
+        super.onCreate();
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId){
+        Log.i("서비스 테스트 ::","onStartCommand() 호출됨");
+        mp = MediaPlayer.create(this, R.raw.nomakeup);
+        mp.setLooping(true);
+        mp.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("서비스 테스트 ::","onDestroy() 호출됨");
+        super.onDestroy();
     }
 }
